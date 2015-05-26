7d23c4169191d7c0dca75833d1ff4e47fcc65863
miwesfx <Miguel@MacBook-Air-de-Miguel.local>
2015-05-26 23:54:44 +0200

    Initial Commit

---
+++ Violencia de género/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Violencia de género
+//
+//  Created by Miguel on 26/5/15.
+//  Copyright (c) 2015 Miguel Pérez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
