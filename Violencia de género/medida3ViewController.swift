367a97e5a963729ec6f215655a59ef33d7e9f4d3
miwesfx <Miguel@MacBook-Air-de-Miguel.local>
2015-06-29 13:59:18 +0200

    Fotos y textos
    Añadidas fotos que faltaban y corregidos muchos textos

--- Violencia de género/medida3ViewController.swift
+++ Violencia de género/medida3ViewController.swift
@@ -1,31 +1,18 @@
 //
-//  MCRViewController.swift
+//  medida3ViewController.swift
 //  Violencia de género
 //
-//  Created by Miguel on 17/6/15.
+//  Created by Miguel on 28/6/15.
 //  Copyright (c) 2015 Miguel Pérez. All rights reserved.
 //
 
 import UIKit
-import MediaPlayer
 
-class MCRViewController: UIViewController {
+class medida3ViewController: UIViewController {
 
-    var moviePlayer:MPMoviePlayerController!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var url:NSURL = NSURL(string: "http://iiinternationalsummers.apps-1and1.net/wp-content/uploads/2015/06/suelta.mov")!
-        
-        moviePlayer = MPMoviePlayerController(contentURL: url)
-        moviePlayer.view.frame = CGRect(x: 0, y: 60, width: self.view.frame.size.width, height: 300)
-        
-        self.view.addSubview(moviePlayer.view)
-        moviePlayer.fullscreen = true
-        
-        moviePlayer.controlStyle = MPMovieControlStyle.Embedded
-        
         // Do any additional setup after loading the view.
     }
 
